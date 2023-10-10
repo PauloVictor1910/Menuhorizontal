96e7a82fa13cb2322ae349e60b3ed1f56440447a
PauloVictor1910 <128413664+PauloVictor1910@users.noreply.github.com>
2023-10-10 10:29:04 -0300

    Initial commit

---
+++ src/Menu.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Menu.css";
+
+const Menu = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Sobre</a>
+        </li>
+        <li>
+          <a href="#">Contato</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
